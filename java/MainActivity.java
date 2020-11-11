26d849849c74a06a1943592e3169418f7da09a46
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2020-11-10 23:49:11 -0800

    Update MainActivity.java

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -18,3 +18,36 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.example.easyshare.DeviceListFragment.DeviceActionListener;
+
+/*An activity that uses WiFi Direct APIs to discover and connect with available devices. WiFi Direct APIs are asynchronous and rely on callback mechanism using interfaces
+to notify the application of operation success or failure.The application should also register a BroadcastReceiver for notification of WiFi state related events.*/
+
+public class MainActivity extends AppCompatActivity implements ChannelListener, DeviceActionListener {
+
+
+    public static final String TAG = "WiFiShareFilesExampleAndroid";
+    private WifiP2pManager manager;
+    private boolean isWifiP2pEnabled = false;
+    private boolean retryChannel = false;
+    private final IntentFilter intentFilter = new IntentFilter();
+    private Channel channel;
+    private BroadcastReceiver receiver = null;
+
+
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // add necessary intent values to be matched.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+    }
